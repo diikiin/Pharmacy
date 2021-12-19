765a712021da3bd44044827e6a13d8090544851e
Dauren <dauren280601@gmail.com>
2021-12-19 23:44:18 +0500

    Commit

--- account/urls.py
+++ account/urls.py
@@ -16,25 +16,20 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
     path('edit-profile/', update_profile, name='edit'),
     path('orders/', orders, name='orders'),
-
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'),
          name='password_change_done'),
-
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='account/password_change.html'),
          name='password_change'),
-
     path('password_reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_done.html'),
          name='password_reset_done'),
-
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'),
          name='password_reset_confirm'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='account/password_reset.html', email_template_name='account/password_reset_email.html', subject_template_name='account/password_reset_subject.txt'),
          name='password_reset'),
-
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'),
-         name='password_reset_complete'),
+         name='password_reset_complete')
 ]
